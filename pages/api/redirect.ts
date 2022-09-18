211ffc7173f6351d734063b45a0e403db3c083cd
Ian Langworth <ian@langworth.com>
2022-09-17 21:12:18 -0700

    No no no, this needs to be done from next.config.js of course

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -1,10 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const path =
-    req.url === "/" || !req.query.path ? "/item?id=30181167" : req.url
   res
     .status(302)
-    .setHeader("Location", `https://news.ycombinator.com${path}`)
+    .setHeader("Location", `https://news.ycombinator.com${req.url}`)
     .end()
 }
