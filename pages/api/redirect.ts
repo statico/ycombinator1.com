68ac7df3a4bd2792dce5db0dc07bc3d49298937f
Ian Langworth <ian@langworth.com>
2022-09-17 21:07:11 -0700

    More robust default path detection

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const path = req.url === "/" ? "/item?id=30181167" : req.url
+  const path =
+    req.url === "/" || !req.query.path ? "/item?id=30181167" : req.url
   res
     .status(302)
     .setHeader("Location", `https://news.ycombinator.com${path}`)
