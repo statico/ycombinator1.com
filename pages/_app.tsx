b5a23f457a9f344d1d84de3e019434bfc6aa5be6
Ian Langworth <ian@langworth.com>
2025-12-09 22:53:51 -0800

    Remove unused _app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,6 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://cdn.tailwindcss.com"></script>
-        <script
-          src="https://s.langworth.com/data.js"
-          data-token="decfacac"
-          defer
-        ></script>
       </Head>
       <Component {...pageProps} />
     </>
